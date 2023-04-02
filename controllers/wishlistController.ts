fc7587bbf29532d3ae8e91eaf6d1df9780c1bacc
simon mor <simomor@gmail.com>
2023-04-02 23:37:37 +0300

    .

--- controllers/wishlistController.ts
+++ controllers/wishlistController.ts
@@ -14,6 +14,12 @@ export const getWishlist = async (req: Request, res: Response) => {
     }
 };
 
+async function bookExistsInWishlist(book: BookI, currentBookIds: string[], newBookIds: string[]) {
+    const existingBook = await BookModel.findOne({ id: book.id });
+    const bookId = existingBook?._id.toString();
+    return currentBookIds.includes(bookId) || newBookIds.includes(bookId);
+}
+
 export const addBookToWishlist = async (req: Request, res: Response) => {
     const { userId } = req.params;
     const { books } = req.body;
@@ -23,13 +29,31 @@ export const addBookToWishlist = async (req: Request, res: Response) => {
 
         if (!wishlist) {
             // Wishlist doesn't exist, create a new one and add the books
-            const bookIds = await Promise.all(
-                books.map(async (book) => {
-                    const newBook = new BookModel(book);
+            const bookIds = [];
+
+            for (const book of books) {
+                // Check if the book already exists in the database
+                const existingBook = await BookModel.findOne({ id: book.id });
+
+                if (!existingBook) {
+                    // Book does not exist, create and save it
+                    const newBook = new BookModel({
+                        title: book.title,
+                        author: book.author,
+                        imageUrl: book.imageUrl,
+                        description: book.description,
+                        publishedDate: book.publishedDate,
+                    });
                     await newBook.save();
-                    return newBook._id;
-                })
-            );
+                    bookIds.push(newBook._id);
+                } else {
+                    // Book already exists, use its ID if it's not in the wishlist yet
+                    const bookId = existingBook._id.toString();
+                    if (!wishlist.books.includes(bookId)) {
+                        bookIds.push(existingBook._id);
+                    }
+                }
+            }
 
             const newWishlist = new WishlistModel({
                 userId,
@@ -39,22 +63,31 @@ export const addBookToWishlist = async (req: Request, res: Response) => {
             return res.status(200).send(newWishlist);
         } else {
             // Wishlist exists, get the current books array
-            const currentBookIds = wishlist.books;
+            const currentBookIds = wishlist.books.map(book => book.toString());
 
             // Build a new books array from the input and from mongo, making sure there are no duplicates
-            const newBookIds = [
-                ...currentBookIds,
-                ...books
-                    .filter((book) => !currentBookIds.includes(book._id))
-                    .map((book) => new BookModel(book))
-                    .map(async (newBook) => {
-                        await newBook.save();
-                        return newBook._id;
-                    }),
-            ];
+            const newBookIds = [];
+
+            for (const book of books) {
+                const existingBook = await BookModel.findOne({ id: book.id });
+
+                if (existingBook && !currentBookIds.includes(existingBook._id.toString()) && !newBookIds.includes(existingBook._id)) {
+                    newBookIds.push(existingBook._id);
+                } else if (!existingBook) {
+                    const newBook = new BookModel({
+                        title: book.title,
+                        author: book.author,
+                        imageUrl: book.imageUrl,
+                        description: book.description,
+                        publishedDate: book.publishedDate,
+                    });
+                    await newBook.save();
+                    newBookIds.push(newBook._id);
+                }
+            }
 
             // Update the wishlist with the new books array
-            wishlist.books = newBookIds;
+            wishlist.books = [...currentBookIds, ...newBookIds];
             const updatedWishlist = await wishlist.save();
             return res.status(200).send(updatedWishlist);
         }
@@ -65,13 +98,14 @@ export const addBookToWishlist = async (req: Request, res: Response) => {
 };
 
 
+
 export const deleteBookFromWishlist = async (req: Request, res: Response) => {
     const { userId, bookId } = req.params;
 
     try {
         const wishlist = await WishlistModel.findOneAndUpdate(
             { userId },
-            { $pull: { books: { id: bookId } } },
+            { $pull: { books: { _id: { $elemMatch: { _id: bookId } } } } },
             { new: true }
         );
 
@@ -81,3 +115,5 @@ export const deleteBookFromWishlist = async (req: Request, res: Response) => {
         return res.status(500).send('Error deleting book from wishlist');
     }
 };
+
+
